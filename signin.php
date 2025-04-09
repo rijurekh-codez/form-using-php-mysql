383890aea13f867ea49aea706f1e071805b4b157
Rijurekh Ghosh <R100209@codez.co.in>
2025-04-09 16:08:33 +0530

    server side validation

--- signin.php
+++ signin.php
@@ -40,7 +40,7 @@ if (isset($_SESSION['user'])) {
       <button type="submit" class="bg-blue-700 text-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
 
 
-      <p class="mt-2">Don't have an account ? <a href="signup.html" class="font-bold">Sign Up</a></p>
+      <p class="mt-2">Don't have an account ? <a href="signup2.php" class="font-bold">Sign Up</a></p>
     </form>
   </div>
 
